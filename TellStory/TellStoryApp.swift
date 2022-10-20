42e87c934b40750cf2ce993a865e41b233859928
gdemerges <guillaume.demerges@protonmail.com>
2022-10-20 19:53:37 +0200

    Initial Commit

---
+++ TellStory/TellStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellStoryApp.swift
+//  TellStory
+//
+//  Created by Guillaume Demergès on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TellStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
