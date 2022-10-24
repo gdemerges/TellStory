c4847aac6114617b379166ac5aa490844e88db4f
Guillaume Demergès <guillaume.demerges@protonmail.com>
2022-10-24 22:51:47 +0200

    Add error handling

---
+++ TellStory/viewModels/UsersViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  UsersViewModel.swift
+//  TellStory
+//
+//  Created by Guillaume Demergès on 24/10/2022.
+//
+
+import Foundation
+
+class usersViewModel: ObservableObject{
+    @Published var user1 = User(mail: "", username: "", password: "", image: "")
+}
+
+enum UserError: Error {
+    case loginPasswordEmpty
+    case invalidLogin
+    case invalidPassword
+    case passwordNotMatching
+    case loginTaken
+}
